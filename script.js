7e44327810e0de650851974b29c2120cedf1b463
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-06-22 14:03:21 -0300

    set local storage

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const theImg = document.querySelector('img');
+const myButton = document.querySelector('button');
+const myH1 = document.querySelector('h1');
 
 function handleClick(evt) {
   const SrcImg1 = './mozila_logo.webp';
@@ -14,4 +16,19 @@ function handleClick(evt) {
   
 }
 
+function setUserName() {
+  const theName = prompt("Your name: ");
+  localStorage.setItem("name202", theName);
+  myH1.textContent = `Mozila is cool, ${theName}!`;
+}
+
 theImg.addEventListener('click', handleClick);
+
+if (!localStorage.getItem('name202')) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem('name202');
+  myH1.textContent = `Mozila is cool, ${storedName}!`;
+}
+
+myButton.addEventListener('click',()=>setUserName());
